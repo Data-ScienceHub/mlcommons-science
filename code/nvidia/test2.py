d5f5cf5862d9ccf5fdad98dab0f6ccf4136af4fc
Gregor von Laszewski <laszewski@gmail.com>
2022-02-02 19:21:30 -0500

    GPU finding test from tensorflow

--- code/nvidia/test2.py
+++ code/nvidia/test2.py
@@ -1,2 +1,12 @@
 import tensorflow as tf
-tf.config.list_physical_devices("GPU")
+from pprint import pprint
+
+r = tf.config.list_physical_devices("GPU")
+
+pprint (r)
+
+if "physical_device:GPU" not in str(r):
+    print ("Test failed")
+else:
+ print("test passed")
+
